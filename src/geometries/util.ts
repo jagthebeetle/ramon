7776099263d5a8663551e4a04568c3838c99adec
Jonathan Alvarez-Gutierrez <jagthebeetle@gmail.com>
2018-01-12 23:18:53 -0500

    Documentation for util methods

--- src/geometries/util.ts
+++ src/geometries/util.ts
@@ -1,23 +1,26 @@
 import { Color } from 'three';
 
+/** Random integer between a (inclusive) and b (exclusive). */
 export function randInt(a = 0, b = 10) {
     return a + Math.floor(Math.random() * (b - a));
 }
 
-
+/** Array of three random numbers, from -50 to 49 (inclusive). */
 export function randomPoint(): [number, number, number] {
     return [randInt(-50, 50), randInt(-50, 50), randInt(-50, 50)];
 }
 
-
+/** Random number 0-9 inclusive, for small attributes. */
 export function randomScalar() {
     return randInt(0, 10);
 }
 
+/** Random THREE.Color, sampled from entire RGB space. */
 export function randomColor() {
     return new Color(randInt(0x000000, 0x1000000));
 }
 
+/** Returns a function that returns the provided value. */
 export function toFunction<T>(x: T): () => T {
     return () => x;
 }
